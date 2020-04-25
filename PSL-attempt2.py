9a51a39c572651bbe0436945eb7004e6133ae19f
Andrew Chen <chen3137@purdue.edu>
2020-04-25 04:47:41 -0400

    notation and commenting

--- PSL-attempt2.py
+++ PSL-attempt2.py
@@ -1,15 +1,11 @@
-import planetarySoftLanding as psl 
-import cvxpy as cp  
-import numpy as np 
-from math import tan, pi, exp, cos
-from numpy.linalg import norm
-from numpy import dot
+import cvxpy as cp
+import numpy as np
 
-z_0 = np.zeros((100, 1)) #min fuel use contraint or something, should be a function of #t_k
-rho1, rho2 = 10, 20      #for thrust upper/lower bounds, 10 and 20 are just placeholders
+z_0 = np.zeros((100, 1))    # z_0(t) for constant maximum thrust, function of t_k
+rho1, rho2 = 10, 20         # thrust bounds
 
-xInitial = np.array([100, .5, .1, -21, -3, 2, 5500])
-xFinal   = np.array([0, 0, 0, 0, 0, 0, 0])
+X_0 = np.array([100, .5, .1, -21, -3, 2, 5500])     # X(t) = (r(t), r'(t), z(t)
+X_t_N = np.array([0, 0, 0, 0, 0, 0, 0])             # X(t_N), final state constraint
 
 mainVector = cp.Variable((100, 11))
 sVect, eta = mainVector[:, 0:7], mainVector[:, 7:11] 
@@ -26,11 +22,11 @@ uV = sVect[:, 3:6] / cp.norm(dz**2 + dx**2 + dy**2)
 parameters  = cp.sum_squares(s_f[0:3])
 objective   = cp.Minimize(parameters)
 
-constraints = [s_i == xInitial, s_f[0] <= .01, s_f[3:6] <= [.01, .01, .01],          #<--- initial/final conditions
+constraints = [s_i == X_0, s_f[0] <= .01, s_f[3:6] <= [.01, .01, .01],  #<--- initial/final conditions
                #cp.norm(x**2 + y**2) <= z*100,                                       #<--- glideslope contraint
-               cp.norm(zT**2 + yT**2 + xT**2) <= gamma,                              #<--- Thrust magnitude constraint
-               cp.sum( cp.multiply(uV, thrust), axis=1 ) <= .1 * gamma,                                      #<--- Pointing contraint
-               #rho2*exp(-z_0) * (1 - (z_t - z_0))                                                               
+               cp.norm(zT**2 + yT**2 + xT**2) <= gamma,  #<--- Thrust magnitude constraint
+               cp.sum( cp.multiply(uV, thrust), axis=1 ) <= .1 * gamma,  #<--- Pointing contraint
+               #rho2*exp(-z_0) * (1 - (z_t - z_0))
                ]
 
 prob = cp.Problem(objective, constraints)
