43a76347999e61132e56baa397d673869d5d0a30
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-04-26 09:57:08 -0400

    Update PSL-3.py

--- PSL-3.py
+++ PSL-3.py
@@ -5,33 +5,31 @@ from math import tan, pi, exp, cos, log
 from numpy import dot
 from numpy import multiply as mult
 
-alpha, m_0 = 1, 5000
-rho1, rho2 = 10, 20      #for thrust upper/lower bounds, 10 and 20 are just placeholders
-xInitial = np.array([100, 19, -10, -21, -3, 2, log(m_0)])
-g = np.array([0, 0, 0, -3.7114, 0, 0, 0])
-
-mainVector = cp.Variable((50, 11))
-x_k, eta = mainVector[:, 0:7], mainVector[:, 7:11] 
-s_i, s_f, xf, yf  = x_k[0, :], x_k[-1, :], x_k[-1, 1], x_k[-1, 2]
+mainVector = cp.Variable((50, 11)) #50 steps, [stateVector(dim=7), controlVector(dim=4)]
+x_k, eta   = mainVector[:, 0:7], mainVector[:, 7:11] 
+x_0, x_N, xN, yN  = x_k[0, :], x_k[-1, :], x_k[-1, 1], x_k[-1, 2]
 z, x, y, z_t      = x_k[:, 0], x_k[:, 1], x_k[:, 2], x_k[:, 6]
-zT, yT, xT, gamma = eta[:, 0], eta[:, 1], eta[:, 2], eta[:, 3]
-thrust, uV        = eta[:, 0:3], x_k[:, 3:6]
+zT, yT, xT, sigma = eta[:, 0], eta[:, 1], eta[:, 2], eta[:, 3]
+thrust, velo      = eta[:, 0:3], x_k[:, 3:6]
 
-parameters  = cp.sum_squares(s_f[0:3])
+parameters  = cp.sum_squares(x_N[0:3])
 objective   = cp.Minimize(parameters)
-constraints = [s_i == xInitial, s_f[0] <= .01, s_f[3:6] <= [.01, .01, .01]]
+constraints = [x_0 == psl.x_0, x_N[0] == 0, x_N[3:6] == [0, 0, 0], x_N >= log(psl.m_0 - psl.alpha*psl.rho_2*50)]
 
-for t in range(50):
-    x_g = np.sum([.001 * np.dot( np.exp(psl.A * (t-.01*t1)), g     ) for t1 in range(100)], axis=0)
+for t in range(1):
+    x_g = np.sum([.001 * np.dot( np.exp(psl.A * (t-.01*t1)), psl.g ) for t1 in range(100)], axis=0)
     x_c = np.sum([.001 * np.dot( np.exp(psl.A * (1-.01*t1)), psl.B ) for t1 in range(100)], axis=0)
-    x_r = np.dot( np.exp(psl.A * t), xInitial )
-    
-    z_0   = log(m_0 - alpha*rho2*t)
-    tHigh = rho2*np.exp(-z_0) * (1 - (z_0-z_t[t]))
-    tLow  = rho1*np.exp(-z_0) * (1 - (z_0-z_t[t]) + .5*(z_0-z_t[t])**2)
-
-    constraints += [tLow <= gamma[t], gamma[t] <= tHigh, 
-                    x_k[t] == x_g + x_r + cp.matmul(x_c, eta[t, :])]
+    x_r = np.dot( np.exp(psl.A * t), psl.x_0 )
+ 
+    z_0   = log(psl.m_0 - psl.alpha*psl.rho_2*t)
+    sHigh = psl.rho_2*np.exp(-z_0) * (1 - (z_0-z_t[t]))
+    sLow  = psl.rho_1*np.exp(-z_0) * (1 - (z_0-z_t[t]) + .5*(z_0-z_t[t])**2)
+                                          
+    constraints += [sLow <= sigma[t], sigma[t] <= sHigh, 
+                    x_k[t]      == x_r + cp.matmul(x_c, eta[t]) + x_g,
+                    zT[t]**2    <= sigma * cos(45),
+                    z/tan(pi/4) >= cp.norm(x**2 + y**2)
+                    ]
 
 prob = cp.Problem(objective, constraints)
 
@@ -43,5 +41,5 @@ try:
         print("No solution was found.")
      
 except cp.error.SolverError:
-    print("The solver failed.")
+    print("The ECOS solver failed.")
     
