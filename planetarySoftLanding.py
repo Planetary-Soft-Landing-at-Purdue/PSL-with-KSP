0e116754b4f7c3e3a17a5e67a696156c9745f67f
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-05-09 11:06:10 -0400

    Update planetarySoftLanding.py

--- planetarySoftLanding.py
+++ planetarySoftLanding.py
@@ -1,60 +1,29 @@
 import numpy as np
 
-w = [0,0,0]
-m_f = 1505
-m_0 = 1905
-alpha = 4.53e-4
-rho_1 = 4972
-rho_2 = 13260
-r_0 = [1500,200,500]
-rdot_0 = [-75,0,100]
-r_tf = [0,0,0]
-rdot_tf = [0,0,0]
+w = [0,0,0]                 # planet angular velocity    
+m_f = 1505                  # dry mass
+m_0 = 1905                  # wet mass
+alpha = 4.53e-4             # fuel consumption rate kg/N
+rho_1 = 32000               # lower bound on thrust
+rho_2 = 80000               # upper bound on thrust
+r_N = [0,0,0]               # final position, ECEF     
+rdot_N = [0,0,0]            # final velocity, ECEF
 V_max = 0
+theta = 45                  # pointing constraint
 
-E   = np.array([[1,0,0,0,0,0,0], 
-                [0,1,0,0,0,0,0],
-                [0,0,1,0,0,0,0]])
-F   = [0,0,0,0,0,0,1]
-E_u = [[1,0,0,0],
-       [0,1,0,0],
-       [0,0,1,0]]
-E_v = [[0,0,0,1,0,0,0],
-       [0,0,0,0,1,0,0],
-       [0,0,0,0,0,1,0]]
-S_1 = np.array([[0,1,0],
-                [0,0,1]])
+x_0 = np.array([1500, 200, 500, -75, 0, 100, np.log(m_0)])
+g  = np.array([0, 0, 0, -1.62, 0, 0, 0])
 
-w = [0,.002,0]
-g = [-3.7114,0,0]
+s_W = np.array([[(w[2]**2+w[1]**2),-1*w[1]*w[0]     ,-1*w[2]*w[0]     , 0     , 2*w[2],-2*w[1]], 
+                [-1*w[1]*w[0]     ,(w[2]**2+w[0]**2),-1*w[2]*w[1]     ,-2*w[2], 0     , 2*w[0]], 
+                [-1*w[2]*w[0]     ,-1*w[2]*w[1]     ,(w[1]**2+w[0]**2), 2*w[1],-2*w[0], 0     ],
+               ])
 
-A = np.array([[0                ,0                ,0                ,1      ,0      ,0      ,0],
-              [0                ,0                ,0                ,0      ,1      ,0      ,0],
-              [0                ,0                ,0                ,0      ,0      ,1      ,0],
-              [(w[2]**2+w[1]**2),-1*w[1]*w[0]     ,-1*w[2]*w[0]     , 0     , 2*w[2],-2*w[1],0], 
-              [-1*w[1]*w[0]     ,(w[2]**2+w[0]**2),-1*w[2]*w[1]     ,-2*w[2], 0     , 2*w[0],0], 
-              [-1*w[2]*w[0]     ,-1*w[2]*w[1]     ,(w[1]**2+w[0]**2), 2*w[1],-2*w[0], 0     ,0],
-              [0                ,0                ,0                ,0      ,0      ,0      ,0]])
-B = np.array([[0,0,0,0    ],
-              [0,0,0,0    ],
-              [0,0,0,0    ],
-              [1,0,0,0    ],
-              [0,1,0,0    ],
-              [0,0,1,0    ],
-              [0,0,0,alpha]])
-C = np.array([[0,0,0],
-              [0,0,0],
-              [0,0,0],
-              [1,0,0],
-              [0,1,0],
-              [0,0,1],
-              [0,0,0]])
-g = np.array([[-3.7114], 
-              [0      ],
-              [0      ]])
-np.swapaxes(A, 0, 1)
-np.swapaxes(B, 0, 1)
-np.swapaxes(C, 0, 1)
-np.swapaxes(g, 0, 1)
-#np.swapaxes(E, 0, 1)
+A = np.concatenate(( np.zeros((3, 3)), np.identity(3) ), axis=1)
+A = np.concatenate(( A, s_W, np.zeros((1, 6)) ))
+A = np.concatenate(( A, np.zeros((7, 1)) ), axis=1)
+
+B = np.concatenate(( np.zeros((3, 3)), np.identity(3), np.zeros((1, 3)) ), axis=0)
+B = np.concatenate(( B, np.array([[0, 0, 0, 0, 0, 0, -alpha]]).T),         axis=1)
+
 
