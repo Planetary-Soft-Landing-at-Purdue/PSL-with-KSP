21a806f5168df21a0d9444ae952fc55681b2d061
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-05-11 22:38:40 -0400

    Add files via upload

--- planetarySoftLanding.py
+++ planetarySoftLanding.py
@@ -1,18 +1,23 @@
 import numpy as np
+import math
+
+dt = 1                     # time step length (seconds)
 
 w = [0,0,0]                 # planet angular velocity    
 m_f = 1505                  # dry mass
-m_0 = 1905                  # wet mass
-alpha = 4.53e-4             # fuel consumption rate kg/N
-rho_1 = 32000               # lower bound on thrust
-rho_2 = 80000               # upper bound on thrust
+m_0 = 2905                  # wet mass
+#alpha = 4.53e-4             # fuel consumption rate kg/N
+alpha = 4.53e-20            # fuel consumption rate kg/N
+rho_1 = 4972                # lower bound on thrust
+rho_2 = 13260               # upper bound on thrust
 r_N = [0,0,0]               # final position, ECEF     
 rdot_N = [0,0,0]            # final velocity, ECEF
 V_max = 0
-theta = 45                  # pointing constraint
+theta = 180                 # pointing constraint
+gamma = 4*(math.pi)/180
 
-x_0 = np.array([1500, 200, 500, -75, 0, 100, np.log(m_0)])
-g  = np.array([0, 0, 0, -1.62, 0, 0, 0])
+x_0 = np.array([600, 0, 0, 0, 0, 0, np.log(m_0)])
+g  = np.array([-3.7114, 0, 0, 0])
 
 s_W = np.array([[(w[2]**2+w[1]**2),-1*w[1]*w[0]     ,-1*w[2]*w[0]     , 0     , 2*w[2],-2*w[1]], 
                 [-1*w[1]*w[0]     ,(w[2]**2+w[0]**2),-1*w[2]*w[1]     ,-2*w[2], 0     , 2*w[0]], 
@@ -26,4 +31,24 @@ A = np.concatenate(( A, np.zeros((7, 1)) ), axis=1)
 B = np.concatenate(( np.zeros((3, 3)), np.identity(3), np.zeros((1, 3)) ), axis=0)
 B = np.concatenate(( B, np.array([[0, 0, 0, 0, 0, 0, -alpha]]).T),         axis=1)
 
+c = np.array([[1,0,0]])/math.tan(gamma)
 
+S = np.array([[0,1,0],
+              [0,0,1]])
+
+E = np.array([[1,0,0,0,0,0,0],
+              [0,1,0,0,0,0,0],
+              [0,0,1,0,0,0,0]])
+
+F = np.array([[0,0,0,0,0,0,1]])
+
+E_u = np.array([[1,0,0,0],
+                [0,1,0,0],
+                [0,0,1,0]])
+
+E_v = np.array([[0,0,0,1,0,0,0],
+                [0,0,0,0,1,0,0],
+                [0,0,0,0,0,1,0]])
+
+cE = np.matmul(c,E)
+SE = np.matmul(S,E)
