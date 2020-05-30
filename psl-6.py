32b3083d1e3f6899dbfbf942248aab1c576b3824
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-05-29 22:12:55 -0400

    Add files via upload

--- psl-6.py
+++ psl-6.py
@@ -3,7 +3,7 @@ import ecos, time, math
 import numpy as np  
 import scipy as sp
 from math import tan, pi, exp, cos, log, factorial
-from scipy.sparse import csr_matrix
+from scipy.sparse import csc_matrix
 
 startTime = time.time()
 
@@ -43,7 +43,7 @@ def equalityConstraints(tSteps):
 
     b = np.concatenate((b, np.zeros(7)), axis=0)     
     
-    return csr_matrix(A), b.astype('float64')
+    return csc_matrix(A), b.astype('float64')
 
 def socConstraints(tSteps):
     G = np.zeros((0, 11*tSteps))
@@ -62,7 +62,7 @@ def socConstraints(tSteps):
     for k in range(tSteps):
         magThr, conPoi = np.zeros((4, 11*tSteps)), np.zeros((2, 11*tSteps))                     # magnitude/thrust constraints      
         magThr[:, 11*k:11*(k+1)] = thrustMag        
-        conPoi[:, 11*k:11*(k+1)] = pointCons
+        conPoi[:, 11*k:11*(k+1)] = -pointCons
 
         z_0 = log(psl.m_0 - psl.alpha*psl.rho_2 * k*psl.dt)                                     # calculated lower bound for mass loss
         A  =  e7 * (.5 * psl.rho_1*exp(-z_0))**.5
@@ -70,7 +70,6 @@ def socConstraints(tSteps):
         c  =  psl.rho_1*exp(-z_0) * (1 + z_0 + .5*z_0**2)
 
         low_k, high_k = np.zeros((3, 11*tSteps)), np.zeros((1, 11*tSteps))
-
         low   = np.concatenate((-bT/2, bT, A), axis=0)                                          # lower thrust bound
         low_h = np.array([.5*(1-c), .5*(1+c), 0])
         low_k[:, 11*k:11*(k+1)] = low
@@ -78,7 +77,7 @@ def socConstraints(tSteps):
         high   = e11 + psl.rho_2*exp(-z_0)*e7                                                   # upper thrust bound
         high_h = np.array([ psl.rho_2*exp(-z_0) * (1+z_0) ])
         high_k[:, 11*k:11*(k+1)] = high
-
+        
         gSlope = np.zeros((3, 11*tSteps))                                                       # glideslope constraints
         gSlope[:, 11*k:11*k + 3]                    = np.identity(3)
         gSlope[:, 11*(tSteps-1):11*(tSteps-1) + 3] += -1*np.identity(3)
@@ -91,19 +90,19 @@ def socConstraints(tSteps):
         for a in q_k:
             q.append(a)
     
-    finDist   = np.zeros((2, 11*tSteps))
-    finDist_h = np.array([2, 0])
-    finDist[:, 11*(tSteps-1) + 1:11*(tSteps-1) + 3] = np.identity(2)
-    q.append(2)
+    finDist   = np.zeros((3, 11*tSteps))
+    finDist_h = np.array([2, 0, 0])
+    finDist[1:3, 11*(tSteps-1) + 1:11*(tSteps-1) + 3] = np.identity(2)
+    q.append(3)
     
     G = np.concatenate((G, finDist), axis=0)
     h = np.concatenate((h, finDist_h))
     
-    return csr_matrix(-1*G), h, q
+    return csc_matrix(-1*G), h, q
 
 def setMinFunc(tSteps):
     c = np.zeros(11*tSteps)
-    c[11*tSteps-4]
+    c[11*(tSteps-1) + 6] = 1
         
     return c
             
