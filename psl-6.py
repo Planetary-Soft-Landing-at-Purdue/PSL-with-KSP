3a9b125095a030a4a6e5927a17d7beda4a7e4eec
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-06-01 14:57:53 -0400

    Add files via upload

--- psl-6.py
+++ psl-6.py
@@ -43,54 +43,58 @@ def equalityConstraints(tSteps):
     A[4+7*tSteps, 11*(tSteps-1)] = 1                                                 # sets final z-position to zero
     
     print("Setting up equality constraints took: ", time.time()-startTime)
-    '''
-    G_txt = ''
-    for r in range(len(A)):
-        for c in A[r]:
-            if c == 0:
-                G_txt += ' '*7 + '-'
-            else:
-                G_txt += ' '*(8-len(str(round(c, 3)))) + str(round(c, 3))
-        G_txt += '  |' + ' '*(8-len(str(round(b[r], 3)))) + str(round(b[r], 3)) +  '\n'
-    txtFile = open("txtFile.txt", 'w')
-    txtFile.write(G_txt)
-    txtFile.close()
-    '''
+    
+    b[11:18] = np.zeros(7)  # temporary fix to system's z-velo changing by g after one time step
+    
     return csc_matrix(A), b.astype('float64')
 
 def socConstraints(tSteps):
     startTime = time.time()
 
-    q_k   = [3, 2, 4, 2, 3]
+    q_k   = [3, 4, 2, 3]
     dim_k = sum(q_k)
+    l     = 2 * tSteps
+    
     e7, e11, e8 = np.zeros((1, 11)), np.zeros((1, 11)), np.zeros((1, 11))
     e7[0, 6]   = 1
     e8[0, 7]   = 1
     e11[0, 10] = 1
+    nCol = 11*(tSteps-1) # starting column for final tStep
     
-    G = np.zeros((dim_k*tSteps+3, 11*tSteps))
-    h = np.zeros(dim_k*tSteps+3)
+    G = np.zeros((l + dim_k*tSteps+3, 11*tSteps))
+    h = np.zeros(l + dim_k*tSteps+3)
     
     for k in range(tSteps):
-        kRow, kCol = dim_k*k, 11*k
+        kRow, kCol = l + dim_k*k, 11*k          # starting row/column for timestep
         
         z_0 = log(psl.m_0 - psl.alpha*psl.rho_2 * k*psl.dt)       # values used for thrust bounds
         A  =  e7 * (.5 * psl.rho_1*exp(-z_0))**.5
         bT = -psl.rho_1*exp(-z_0) * (e7 + z_0*e7) - e11
         c  =  psl.rho_1*exp(-z_0) * (1 + z_0 + .5*z_0**2)
         
+        G[k, kCol:kCol+11] = -(psl.rho_2*exp(-z_0)*e7 + e11)    # thrust upper bound
+        h[k]               = psl.rho_2*exp(-z_0)*(1 + z_0)
+        G[tSteps+k, kCol:kCol+11] = e8 - e11 * cos(psl.theta)   # pointing constraint, no constant values so h stays as zero
+        
         #G[kRow:kRow+3, kCol:kCol+11] = np.concatenate((-bT/2, bT, A), axis=0)                       # thrust lower bound
         #h[kRow:kRow+3]               = np.array([.5*(1-c), .5*(1+c), 0])
-        G[kRow+4, kCol:kCol+11] = e11 + psl.rho_2*exp(-z_0)*e7                                      # thrust upper bound
-        h[kRow+3]               = psl.rho_2*exp(-z_0) * (1 + z_0)
         
-        G[kRow+5, kCol:kCol+11]          = e11              # thrust magnitude
-        G[kRow+6:kRow+9, kCol+7:kCol+10] = np.identity(3)
-        G[kRow+9, kCol:kCol+11]  = e8                       # pointing constraint
-        G[kRow+10, kCol:kCol+11] = e11 * cos(psl.theta)
+        G[kRow+3, kCol:kCol+11]          = e11              # thrust magnitude
+        G[kRow+4:kRow+7, kCol+7:kCol+10] = np.identity(3)
+        
+        G[kRow+9:kRow+12, kCol:kCol+3]  = np.identity(3)    # glideslope constraint
+        G[kRow+9:kRow+12, nCol:nCol+3] -= np.identity(3)    # no glideslope constraint for final tStep (z=0), if nCol = kCol, no SOC constraint
+        G[kRow+9, kCol] /= tan(psl.gamma)
+        G[kRow+9, nCol] /= tan(psl.gamma)
     
-    G[dim_k*tSteps+1:dim_k*tSteps+3, 11*(tSteps-1)+1:11*(tSteps-1)+3] = np.identity(2)
-    h[dim_k*tSteps] = 10
+    G[l + dim_k*tSteps+1:l + dim_k*tSteps+3, nCol+1:nCol+3] = np.identity(2)
+    h[l + dim_k*tSteps] = 5
+    
+    q = []
+    for k in range(tSteps):
+        for a in q_k:
+            q.append(a)
+    q.append(3)
     '''
     G_txt = ''
     for r in range(len(G)):
@@ -100,31 +104,25 @@ def socConstraints(tSteps):
             else:
                 G_txt += ' '*(8-len(str(round(-c, 3)))) + str(round(-c, 3))
         G_txt += '  |' + ' '*(8-len(str(round(h[r], 3)))) + str(round(h[r], 3)) +  '\n'
-    txtFile = open("txtFile.txt", 'w')
+    txtFile = open("G_matrix.txt", 'w')
     txtFile.write(G_txt)
     txtFile.close()
     '''
-    q = []
-    for k in range(tSteps):
-        for a in q_k:
-            q.append(a)
-    q.append(3)
-    
     print("Setting up SOC constraints took: ", time.time()-startTime)
-    return csc_matrix(-1*G), h, q
+    return csc_matrix(-1*G), h, q, l
 
 def setMinFunc(tSteps):
     c = np.zeros(11*tSteps)
-    c[11*(tSteps-1) + 6] = 0
+    c[11*(tSteps-1) + 6] = -1
         
     return c
             
 tSteps = 80
-G, h, q = socConstraints(tSteps)
-A, b    = equalityConstraints(tSteps)    
-c       = setMinFunc(tSteps)
+G, h, q, l = socConstraints(tSteps)
+A, b       = equalityConstraints(tSteps)    
+c          = setMinFunc(tSteps)
 
-solution = ecos.solve(c, G, h, {'l':0, 'q':q}, A=A, b=b, feastol=.1)
+solution = ecos.solve(c, G, h, {'l':l, 'q':q}, A=A, b=b, feastol=.5, reltol=.5, abstol=.5)
 
 writeData(solution['x'])
 
