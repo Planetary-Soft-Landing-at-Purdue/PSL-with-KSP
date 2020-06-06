291c81fc40c881d2352c2a59b5c2d53be0f34b9b
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-06-06 12:19:28 -0400

    Add files via upload

--- guidance.py
+++ guidance.py
@@ -1,22 +1,22 @@
 import ecos
 import time
 import numpy as np
-from math import tan, pi, exp, cos, log, factorial
+from math import tan, pi, exp, cos, log, factorial, sqrt
 from scipy.sparse import csc_matrix
 
 # here we define some constants that are used throughout the program
 
-dt = .25                     # time step length (seconds)
-w = [2.53e-5, 0, 6.62e-5]   # planet angular velocity
-m_f = 1505                  # dry mass
-m_0 = 1905                  # wet mass
-alpha = 4.53e-4             # fuel consumption rate kg/N
-rho_1 = 4972                # lower bound on thrust
-rho_2 = 13260               # upper bound on thrust
-theta = pi / 2                # pointing constraint
-gamma = pi / 3                # glideslope constraint
+dt = .1                         # time step length (seconds)
+w = [2.53e-5, 0, 6.62e-5]       # planet angular velocity
+m_f = 1505                      # dry mass
+m_0 = 1905                      # wet mass
+alpha = 4.53e-4                 # fuel consumption rate kg/N
+rho_1 = 4972                    # lower bound on thrust
+rho_2 = 13260                   # upper bound on thrust
+theta = pi / 2                  # pointing constraint
+gamma = pi / 3                  # glideslope constraint
   
-x_0 = np.array([1500, 400, -220, 0, 0, 0, np.log(m_0), 0, 0, 0, 0])
+x_0 = np.array([4000, 400, -220, 0, 0, 0, np.log(m_0), 0, 0, 0, 0])
 g = np.array([0, 0, 0, 0, 0, 0, 0, -3.7114, 0, 0, 0])
 
 s_W = np.array([[(w[2]**2 + w[1]**2), -1 * w[1] * w[0], -1 * w[2] * w[0], 0, 2 * w[2], -2 * w[1]],
@@ -54,26 +54,27 @@ def matExp(A, x):
     return expMat
 
 
+phi = matExp(A, dt)
+psi = np.trapz([np.dot(matExp(A, tau * .0005 * dt), B)
+                for tau in range(2000)], axis=0, dx=.0005 * dt)
+omega = np.concatenate((phi, psi), axis=1)
+E = np.concatenate((-omega, np.identity(7), np.zeros((7, 4))), axis=1)
+
+rowList, colList, valList = [], [], []
+
+for r in range(len(E)):
+    for c in range(len(E[0])):
+        if E[r, c] != 0:
+            rowList.append(r)
+            colList.append(c)
+            valList.append(E[r, c])
+
+
 def equalityConstraints(tSteps):
     startTime = time.time()
     
-    psi = matExp(A, dt)
-    phi = np.trapz([np.dot(matExp(A, tau * .002), B)
-                    for tau in range(500)], axis=0, dx=.002)
-    omega = np.concatenate((psi, phi), axis=1)
-    E = np.concatenate((-omega, np.identity(7), np.zeros((7, 4))), axis=1)
-    
-    rowList, colList, valList = [], [], []
-
-    for r in range(len(E)):
-        for c in range(len(E[0])):
-            if E[r, c] != 0:
-                rowList.append(r)
-                colList.append(c)
-                valList.append(E[r, c])
-    
     n_k, nVals = len(rowList), len(rowList) * tSteps + 16
-    A_row, A_col, A_val = np.zeros(nVals+1), np.zeros(nVals+1), np.zeros(nVals+1)
+    A_row, A_col, A_val = np.zeros(nVals), np.zeros(nVals), np.zeros(nVals)
     A_row[0:11], A_col[0:11] = np.linspace(0, 10, 11), np.linspace(0, 10, 11)
     A_val[0:11] = np.ones(11)
     
@@ -99,12 +100,10 @@ def equalityConstraints(tSteps):
     for t in range(tSteps - 1):
         b[11 + t * 7: 18 + t * 7] = bVect
     b[11:18] = np.zeros(7)
-    
-    A_row[-1], A_col[-1] = 14 + 7 * tN, 11 + 11 * tN
-    
+        
     print("Setting up equality constraints took: ", time.time() - startTime)
 
-    return csc_matrix((A_val, (A_row, A_col)), shape=(11 + 7 * tSteps, 1 + 11 * tSteps)), b.astype('float64')
+    return csc_matrix((A_val, (A_row, A_col)), shape=(11 + 7 * tSteps, 11 * tSteps)), b.astype('float64')
 
 
 def socConstraints(tSteps):
@@ -113,44 +112,52 @@ def socConstraints(tSteps):
     q_k = [3, 4, 3]
     dim_k = sum(q_k)
     l = 2 * tSteps + 1
-    
-    e7, e8, e11 = np.zeros((1, 11)), np.zeros((1, 11)), np.zeros((1, 11))
-    e7[0, 6] = 1
-    e8[0, 7] = 1
-    e11[0, 10] = 1
-    nCol = 11 * (tSteps - 1)
+    nCol, nRow = 11 * (tSteps - 1), l + dim_k * tSteps
 
-    G = np.zeros((l + dim_k * tSteps + 3, 11 * tSteps + 1))
     h = np.zeros(l + dim_k * tSteps + 3)
 
-    G[0, 11*(tSteps-1):11*tSteps] = e7
+    G_row, G_col, G_val = [0], [nCol + 7], [1]
     h[0] = log(m_f)
     
-    for i in range(tSteps):
-        k, kRow, kCol = i + 1, l + dim_k * i, 11 * i
+    for t in range(tSteps):
+        k, kRow, kCol = t + 1, l + dim_k * t, 11 * t
         
-        z_0 = log(m_0 - alpha * rho_2 * k * dt)
-        A = e7 * (.5 * rho_1 * exp(-z_0))**.5
-        bT = -rho_1 * exp(-z_0) * (e7 + z_0 * e7) - e11
-        c = rho_1 * exp(-z_0) * (1 + z_0 + .5 * z_0**2)
-
-        G[k, kCol:kCol + 11] = -(rho_2 * exp(-z_0) * e7 + e11)
+        z_0 = log(m_0 - alpha * rho_2 * t * dt)
+        c   = 1 + z_0 + .5 * z_0**2
+        
+        G_row.extend([k, k])
+        G_col.extend([kCol + 6, kCol + 10])
+        G_val.extend([-rho_2 * exp(-z_0), -1])
         h[k] = rho_2 * exp(-z_0) * (1 + z_0)
-        G[tSteps + k, kCol:kCol + 11] = e8 - e11 * cos(theta)
         
-        G[kRow:kRow + 3, kCol:kCol +
-            11] = np.concatenate((-bT / 2, bT / 2, A), axis=0)
-        h[kRow:kRow + 3] = np.array([.5 * (1 - c), .5 * (1 + c), 0])
-
-        G[kRow + 3, kCol:kCol + 11] = e11
-        G[kRow + 4:kRow + 7, kCol + 7:kCol + 10] = np.identity(3)
-
-        G[kRow + 7:kRow + 10, kCol:kCol + 3] = np.identity(3)
-        G[kRow + 7:kRow + 10, nCol:nCol + 3] -= np.identity(3)
-        G[kRow + 7, kCol] /= tan(gamma)
-        G[kRow + 7, nCol] /= tan(gamma)
+        G_row.extend([tSteps+k, tSteps+k])
+        G_col.extend([kCol + 7, kCol + 10])
+        G_val.extend([1, -cos(theta)])
         
-    G[l + dim_k * tSteps + 1:l + dim_k * tSteps + 3, nCol + 1:nCol + 3] = np.identity(2)
+        #G_row.extend([kRow, kRow, kRow+1, kRow+1, kRow+2])    
+        #G_col.extend([kCol+6, kCol+10, kCol+6, kCol+10, kCol+6])    
+        #G_val.extend([ .5 * (1 + z_0),  .5 / (rho_1 * exp(-z_0)),
+        #              -.5 * (1 + z_0), -.5 / (rho_1 * exp(-z_0)),
+        #              1 / (sqrt(2))
+        #              ])
+        #h[kRow:kRow + 3] = [.5 * (1 - c), .5 * (1 + c), 0]
+
+        G_row.extend([kRow+3, kRow+4, kRow+5, kRow+6])
+        G_col.extend([kCol+10, kCol+7, kCol+8, kCol+9])
+        G_val.extend([1, 1, 1, 1])
+        
+        if t != tSteps - 1:  
+            G_row.extend(range(kRow+7, kRow+10))
+            G_col.extend(range(kCol, kCol+3))
+            G_val.extend([1/tan(gamma), 1, 1])
+            
+            G_row.extend(range(kRow+7, kRow+10))
+            G_col.extend(range(nCol, nCol+3))
+            G_val.extend([-1/tan(gamma), -1, -1])
+        
+    G_row.extend([nRow + 1, nRow + 2])
+    G_col.extend([nCol + 1, nCol + 2])
+    G_val.extend([1, 1])
     h[l + dim_k * tSteps] = 5
 
     q = []
@@ -160,12 +167,15 @@ def socConstraints(tSteps):
     q.append(3)
 
     print("Setting up SOC constraints took: ", time.time() - startTime)
-    return csc_matrix(-1 * G), h, q, l
+    
+    G_val = [-g for g in G_val]
+    
+    return csc_matrix((G_val, (G_row, G_col)), shape=(l + dim_k * tSteps + 3, 11 * tSteps)), h, q, l
 
 
 def setMinFunc(tSteps):
-    c = np.zeros(11 * tSteps + 1)
-    c[-1] = 0
+    c = np.zeros(11 * tSteps)
+    c[11*(tSteps - 1) + 6] = 0
 
     return c
 
@@ -174,9 +184,11 @@ def runEcos(tSteps, getAns=False):
     G, h, q, l = socConstraints(tSteps)
     A_mat, b = equalityConstraints(tSteps)
     c = setMinFunc(tSteps)
-
+    
+    #solution = ecos.solve(
+    #    c, G, h, {'l': l, 'q': q}, A=A_mat, b=b, feastol=.001, abstol=.001, reltol=.001)
     solution = ecos.solve(
-        c, G, h, {'l': l, 'q': q}, A=A_mat, b=b, feastol=.05, abstol=.05, reltol=.05)
+        c, G, h, {'l': l, 'q': q}, A=A_mat, b=b)
 
     if getAns == False:
         return solution['info']['exitFlag'], -solution['x'][11 * (tSteps - 1) + 6]
@@ -187,20 +199,17 @@ def runEcos(tSteps, getAns=False):
 if __name__ == "__main__":
     zeta = .618
     tLow = 0
-    tHigh = 500
-    exitCond = .001
-    count = 0
+    tHigh = 1500
 
     t1 = int(tHigh - zeta * (tHigh - tLow))
     t2 = int(tLow + zeta * (tHigh - tLow))
     inf_1, f_t1 = runEcos(t1)
     inf_2, f_t2 = runEcos(t2)
     
-    while (inf_1 != 0 or inf_2 != 0 or abs(f_t1 - f_t2) >= exitCond) and count < 15:
-        print(t1, t2)
+    while (inf_1 != 0 and inf_1 != 10) or (inf_2 != 0 and inf_2 != 10) or abs(t1 - t2) > 1:
         moveRight = False
 
-        if inf_1 != 0 or (inf_2 == 0 and f_t1 > f_t2):
+        if (inf_1 != 0 and inf_1 != 10) or ((inf_2 == 0 or inf_2 == 10) and f_t1 > f_t2):
             moveRight = True
 
         if moveRight == True:
@@ -218,11 +227,12 @@ if __name__ == "__main__":
 
             inf_2, f_t2 = inf_1, f_t1
             inf_1, f_t1 = runEcos(t1)
-        count += 1
-    
+        print(t1, t2)
+        
     tFinal = int(.5 * float(t1 + t2))
 
-    print(tFinal)
     solution = runEcos(tFinal, getAns=True)
     writeData(solution, tFinal)
+    
+    print("Total time steps: ", tFinal, "\n")
 
