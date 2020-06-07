bf986f767c70f488d4ecda50e4eb6b0a48660926
Andrew Chen <andrewchen0028@gmail.com>
2020-06-07 17:55:24 -0400

    add try/except met_stream simulator

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-from multiprocessing import Value
+from multiprocessing import Value,Array
 
 met = Value('d')
 met.value = 0
 
 new_eta = Value('i')
 new_eta.value = 1
+
+eta = Array('d', 0)
--- pdg_main.py
+++ pdg_main.py
@@ -7,12 +7,20 @@ def process_time():
 	#  connect to server, set conn & vessel
 	#  create & start MET stream, run update_met on callback
 	#  wait for termination
-	conn = krpc.connect(address='192.168.1.169')
-	vessel = conn.space_center.active_vessel
-	met_stream = conn.add_stream(getattr,vessel,'met')
-	met_stream.add_callback(update_met)
-	met_stream.start()
-	while True: pass
+	try:
+		conn = krpc.connect(address='192.168.1.169')
+		vessel = conn.space_center.active_vessel
+		met_stream = conn.add_stream(getattr,vessel,'met')
+		met_stream.add_callback(update_met)
+		met_stream.start()
+		while True: pass
+	#  if connection fails, simulate MET updates
+	except:
+		print('Connection failed, simulating MET')
+		while True:
+			update_met(met.value)
+			time.sleep(0.04)
+			met.value += 0.04
 
 def update_met(self):
 	#  update MET and runs callback-synced functions
@@ -28,9 +36,8 @@ def control():
 		cet = 0
 		new_eta.value = 0
 	cet = met.value-cet0
-	lock.acquire()
-	print('--- cet:',round(cet,3))
-	lock.release()
+	with lock:
+		print('--- cet:',round(cet,3))
 
 
 def process_guidance():
@@ -42,9 +49,10 @@ def process_guidance():
 		t0 = met.value
 		time.sleep(0.25)
 		dT = met.value-t0
-		lock.acquire()
-		print('GdT:',round(dT,3),';','T:',round(T,2))
-		lock.release()
+		with lock:
+			print('GdT:',round(dT,3),';','T:',round(T,2))
+			eta = [T,T]
+			print(eta[:])
 		T -= dT
 		new_eta.value = 1
 	process_time.terminate()
