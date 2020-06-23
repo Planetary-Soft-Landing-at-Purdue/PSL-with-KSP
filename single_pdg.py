09790a45036b3d4c745939e4a74b17f7479d20dc
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-06-23 16:03:24 -0400

    Add files via upload

--- single_pdg.py
+++ single_pdg.py
@@ -40,10 +40,10 @@ print("Started all streams")
 print("Starting Launch")
 
 vessel.auto_pilot.sas = True
-vessel.control.throttle = .75
+vessel.control.throttle = .66
 vessel.control.activate_next_stage()
 
-while vessel.flight().surface_altitude < 500: pass
+while vessel.flight().surface_altitude < 600: pass
 
 vessel.control.throttle = 0
 vessel.auto_pilot.sas 	= False
@@ -55,11 +55,11 @@ stateVect = np.array([
 	-velocity_stream()[1], velocity_stream()[0], velocity_stream()[2],
 	np.log(mass_stream()), 0, 0, 0, 0])
 
-dt = .5
+dt = 1
 
 print("Looking for solution")
 tWait, tSolve, dMax = pdg.PDG(dt, stateVect, initialSearch=True)
-eta = pdg.PDG(dt, stateVect, tWait=tWait, tSolve=tSolve, dMax=dMax)
+eta, sol = pdg.PDG(dt, stateVect, tWait=tWait, tSolve=tSolve, dMax=dMax)
 print("Found solution")
 
 '''
@@ -70,26 +70,45 @@ print("Found solution")
 	the vessel's thrust to a new vector from eta.
 '''
 
+realSol = []
+
 vessel.auto_pilot.engage()
 vessel.auto_pilot.reference_frame = pcpf
 
-while vessel.flight().surface_altitude > 500: pass
+while vessel.flight().surface_altitude > 600: pass
 print("Starting controlled descent")
-startTime, n, tPrev = time.time(), 4, 0
+startTime, n, tPrev = time.time(), 0, 0
 
-while vessel.flight().surface_altitude > 5:
+while vessel.flight().surface_altitude > 5 and n < len(eta) - 4:
 	# waits until a there's a new discrete time step before updating the
 	# vessel's thrust
 	while int( (time.time() - startTime) / dt ) == tPrev: pass
-	
+
 	n, tPrev, mass = n + 4, int( (time.time() - startTime) / dt ), mass_stream()
 
 	vessel.auto_pilot.target_direction = eta[n+1], eta[n], eta[n+2]
+	vessel.control.throttle 		   = eta[n+3] * (mass / pdg.rho_2)
 
-	throttle = eta[n+3]*mass/pdg.rho_2
-	if throttle < 0.36: 
-		vessel.control.throttle = 0.36
-	else: 
-		vessel.control.throttle = eta[n+3] * (mass / pdg.rho_2)
+	pos  = position_stream()
+	velo = velocity_stream()
+	realSol.extend([pos[1], pos[0], pos[2], velo[1], velo[0], velo[2], np.log(mass), eta[n]*eta[n+3], eta[n+1]*eta[n+3], eta[n+2]*eta[n+3], eta[n+3]])
 
 vessel.control.throttle = 0
+
+# here we write two files, one containing the control eta 
+# (what its supposed to do), and the other contains the real
+# eta (what it actually does)
+
+pdgDataFile = open("dataFile_control.txt", 'w')
+pdgString = ""
+for s in sol:
+	pdgString += str(s) + ','
+pdgDataFile.write(pdgString)
+pdgDataFile.close()
+
+realDataFile = open("dataFile_real.txt", "w")
+realString = ""
+for s in realSol:
+	realString += str(s) + ','
+realDataFile.write(realString)
+realDataFile.close()
