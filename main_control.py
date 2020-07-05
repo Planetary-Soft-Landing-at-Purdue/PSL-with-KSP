b1eca262d84407881cd57d7bb0e3ede76e840df7
John Bensen <56515283+JohnBensen1000@users.noreply.github.com>
2020-07-05 14:58:02 -0400

    increases min landing distances for small tSolve
    At tSolve < 6, pdg.dMax = 2x current horizontal distance. This prevents the vessel from trying to reach the origin right before it lands, allowing it to land vertically

--- main_control.py
+++ main_control.py
@@ -40,10 +40,10 @@ def process_time():
 	mass_stream.start()
 	met_stream.start()
 
-	# launches the vessel, waits until it reaches a certain 
-	# altitude, then calls pdg and begins descent
 	print("Made connections, launching")
 
+	# launches the vessel, waits until it reaches a certain altitude, then cuts engines 
+	# and waits until vertical velocity hits zero, then starts guided descent
 	vessel.auto_pilot.sas = True
 	vessel.control.throttle = .6
 	vessel.control.activate_next_stage()
@@ -59,17 +59,26 @@ def process_time():
 	print("Starting controlled descent")
 	startTime, n, tPrev, ns.eta = met_stream(), 0, -1, [0, 0, 0, 0]
 
-	realSol, pdgSol = [], []
-	ns.predTime     = 1
+	# realSol and pdgSol are lists that keep track of the actual state vector at every time step and pdg-predicted 
+	# state vector at every time step. predTime is the time offset from the when pdg is called and when it finishes solving.
+	realSol, pdgSol    = [], []
+	ns.predTime, ns.dt = 1, 1
 
 	# continuously updates the vessel's thrust until it reaches a certain
 	# altitude above its target landing spot
-	while ns.new_eta == None or position_stream()[1] > 1:
-		# updates ns namespace variables
+	while ns.new_eta == None or position_stream()[1] > 2:
+		# predicts what the state vector will be when the next pdg call finishes, pdg solves for this state vector
 		pos, velo, mass = position_stream(), velocity_stream(), mass_stream()
 		ns.predPos  = [pos[0]  + ns.predTime * velo[0],     pos[1]  + ns.predTime * velo[1],               pos[2]  + ns.predTime * velo[2]     ]
 		ns.predVelo = [velo[0] + ns.predTime * ns.eta[n+1], velo[1] + ns.predTime * (ns.eta[n] + ns.g[7]), velo[2] + ns.predTime * ns.eta[n+2] ]
-		ns.nextEta  = ns.eta[n], ns.eta[n+1], ns.eta[n+2], ns.eta[n+3] 
+		#ns.nextEta  = ns.eta[n], ns.eta[n+1], ns.eta[n+2], ns.eta[n+3] 
+		# if there is a next eta, send the next eta, if no next eta, send current eta
+		nextEta = n + 4 * int(ns.predTime / ns.dt)
+		if nextEta < len(ns.eta):
+			ns.nextEta = ns.eta[nextEta: nextEta + 4]
+		else:
+			ns.nextEta = ns.eta[-4:]
+
 		ns.predMass = mass_stream()
 		ns.met      = met_stream()
 
@@ -87,11 +96,13 @@ def process_time():
 			if int((met_stream() - startTime) / ns.dt) != tPrev: 
 				n, tPrev = n + 4, int((met_stream() - startTime) / ns.dt)
 
+				# adds real state vector and pdg state vector to realSol and pdgSol
 				pdgSol.extend(ns.sol[(n // 4) * 11 : (n // 4) * 11 + 11])
 				realSol.extend([position_stream()[1], position_stream()[0], position_stream()[2], 
 					velocity_stream()[1], velocity_stream()[0], velocity_stream()[2], np.log(mass_stream()),
 					ns.eta[n], ns.eta[n+1], ns.eta[n+2], ns.eta[n+3]])
 
+			# sets vessel direction and throttle based on current eta
 			vessel.auto_pilot.target_direction = ns.eta[n+1], ns.eta[n], ns.eta[n+2]
 			vessel.control.throttle 		   = ns.eta[n+3] * (mass_stream() / ns.rho_2)
 
@@ -113,6 +124,7 @@ def process_guid():
 	time.sleep(1)
 	ns.new_eta = None
 
+	# vessel specific constants
 	m_f   = 9495
 	rho_1 = 936508 * .15
 	rho_2 = 936508 * .4
@@ -124,6 +136,7 @@ def process_guid():
 
 	# waits until process time tells it to start looking for a solution
 	while ns.startPDG == False: pass
+
 	pdg.init_constants(ns.g, ns.w)
 	pdg.dt, ns.dt, ns.rho_2 = 1, 1, pdg.rho_2
 	pdg.update_state_vector(ns.predPos, ns.predVelo, ns.predMass, ns.nextEta)
@@ -131,25 +144,43 @@ def process_guid():
 	tSolve        = pdg.PDG(findTimeSolve=True)
 	print("Found tSolve")
 	pdg.dt, ns.dt = .1, .1
+	initialSol    = True
 
 	while ns.startPDG:
+		# calls pdg to optimize fuel use and recalculate path, tells process time
+		# that there is a new solution
+
 		startTime = ns.met
 
 		# update state vector
 		pdg.update_state_vector(ns.predPos, ns.predVelo, ns.predMass, ns.nextEta)
 
-		# calls pdg to optimize fuel use and recalculate path, tells process time
-		# that there is a new solution
-		ns.sol, ns.eta = pdg.PDG(tSolve=tSolve)
-		print("----", tSolve)
+		# if time runs out, add two seconds to tSolve
+		if tSolve < 1.0: tSolve += 2.0
+		# if tSolve is less than 6.0, replace min distance constraint with 2x current
+		# distance, allows the vessel to do a vertical descent landing
+		if tSolve < 6.0:
+			pdg.dMax = 2 * np.linalg.norm(pdg.x[1:3])
 
-		ns.new_eta  = True
+		ns.sol, ns.eta = pdg.PDG(tSolve=tSolve)
+		print("----", tSolve, pdg.dMax)
+
+		# tells process time that there is a new solution, decrement tSolve based on how much 
+		# time has passed, and adjust predTime based on how long the prev pdg took to solve
 		tSolve     -= ns.met - startTime
 		ns.predTime = ns.met - startTime
-	
-		#if tSolve < tSolveTotal / 4 : ns.dt = .1
-		#if tSolve < tSolveTotal / 8 : ns.dt = .05
-		#if tSolve < tSolveTotal / 8: ns.dt = .02
+		ns.new_eta  = True
+
+		if initialSol:
+			initPdg = ns.sol
+			initialSol = False
+
+	initPdgData = open("initial_pdg_data.txt", "w")
+	pdgStr = ''
+	for s in initPdg:
+		pdgStr += str(s) + ","
+	initPdgData.write(pdgStr)
+	initPdgData.close()
 
 	#process_time.terminate()
 
